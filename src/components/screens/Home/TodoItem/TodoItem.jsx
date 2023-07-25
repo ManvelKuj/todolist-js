502dc29de1b992e1b2448285a4192b3308e617ac
ManvelKuj <frontendKujoyan@yandex.ru>
2023-07-25 17:10:49 +0400

    add isCompleted and remove button

--- src/components/screens/Home/TodoItem/TodoItem.jsx
+++ src/components/screens/Home/TodoItem/TodoItem.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
+import cn from 'classnames'
 import { Check } from './Check'
+import { MdDeleteOutline } from 'react-icons/md'
 
-const TodoItem = ({ todo }) => {
+const TodoItem = ({ todo, isCompleted, todoRemove }) => {
+
   return (
-    <div className='flex items-center px-5 py-6 bg-bgitem mb-3 rounded-2xl'>
-      <Check />
-      <div className='text-white'>{todo.title}</div>
+    <div className='flex items-center justify-between px-5 py-6 bg-bgitem mb-3 rounded-2xl w-full'>
+      <button className='flex items-center' onClick={() => isCompleted(todo.id)}>
+        <Check isCompleted={todo.isCompleted} />
+        <div className={cn('text-white', {
+          'line-through': todo.isCompleted
+        })}>{todo.title}</div>
+      </button>
+      <button className=''>
+        <MdDeleteOutline size={26} color='#F7749E' onClick={() => todoRemove(todo.id)} />
+      </button>
     </div>
   )
 }
